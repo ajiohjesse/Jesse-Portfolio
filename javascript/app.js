5eaed30efc143c11341294c18966179c28f3b491
ajiohjesse <ajiohjesse@gmail.com>
2022-07-13 16:38:16 +0100

    add functionality for hero slider

--- javascript/app.js
+++ javascript/app.js
@@ -1,1 +1,19 @@
 
+const slide = document.querySelector(".slide")
+
+let value = 0
+
+setInterval(() => {
+  if (value < 2) {
+    value++
+    changeSlide()
+  } else {
+    value = 0
+    changeSlide()
+  }
+}, 3000)
+
+const changeSlide = () => {
+  slide.style.bottom = `${value}00%`
+}
+
