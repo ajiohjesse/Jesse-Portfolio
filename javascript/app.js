dfc493bf2960ad9c4c6007735d860ae1350c01ff
ajiohjesse <ajiohjesse@gmail.com>
2022-07-14 00:30:11 +0100

    Add about section, create animations for hero and service card.

--- javascript/app.js
+++ javascript/app.js
@@ -1,19 +1,24 @@
 
-const slide = document.querySelector(".slide")
+//=======HERO TEXT SLIDER=======//
+const slide = document.querySelector(".slide");
+if (!document.hidden) {
+  let value = 0;
+  setInterval(() => {
+    if (value < 2) {
+      value++;
+      changeSlide();
+    } else {
+      value = 0;
+      changeSlide();
+    }
+  }, 3000);
 
-let value = 0
+  const changeSlide = () => {
+    slide.style.bottom = `${value}00%`;
+  };
 
-setInterval(() => {
-  if (value < 2) {
-    value++
-    changeSlide()
-  } else {
-    value = 0
-    changeSlide()
-  }
-}, 3000)
-
-const changeSlide = () => {
-  slide.style.bottom = `${value}00%`
+  document.addEventListener("DOMContentLoaded", changeSlide());
+} else {
+  clearInterval()
 }
-
+//======= END HERO TEXT SLIDER=======//
